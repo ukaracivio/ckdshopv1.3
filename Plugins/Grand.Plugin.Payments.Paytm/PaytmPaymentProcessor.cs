0a0c570e32437dc69fafcc8e211213133616a5a1
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2020-12-10 06:33:21 +0300

    Last Compile

--- Plugins/Grand.Plugin.Payments.Paytm/PaytmPaymentProcessor.cs
+++ Plugins/Grand.Plugin.Payments.Paytm/PaytmPaymentProcessor.cs
@@ -1,7 +1,7 @@
 using Grand.Core;
-using Grand.Core.Domain.Directory;
-using Grand.Core.Domain.Orders;
-using Grand.Core.Domain.Payments;
+using Grand.Domain.Directory;
+using Grand.Domain.Orders;
+using Grand.Domain.Payments;
 using Grand.Core.Plugins;
 using Grand.Plugin.Payments.Paytm.Controllers;
 using Grand.Services.Common;
